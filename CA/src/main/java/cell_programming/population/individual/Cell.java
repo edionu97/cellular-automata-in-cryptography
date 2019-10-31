84f26aa35f0ff67bbe4aeacae8768effb31143cd
Eduard Onu <Eduard.Onu@endava.com>
2019-10-31 10:30:57 +0200

    added cell

--- CA/src/main/java/cell_programming/population/individual/Cell.java
+++ CA/src/main/java/cell_programming/population/individual/Cell.java
@@ -4,4 +4,41 @@ public class Cell {
 
     private Rule cellRule;
 
+    private int cellIndex;
+
+    private StringBuilder configuration;
+
+    public Rule getCellRule() {
+        return cellRule;
+    }
+
+    public void setCellRule(Rule cellRule) {
+        this.cellRule = cellRule;
+    }
+
+    public StringBuilder getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(final StringBuilder configuration) {
+        this.configuration = configuration;
+    }
+
+    public int getCellIndex() {
+        return cellIndex;
+    }
+
+    public void setCellIndex(final int cellIndex) {
+        this.cellIndex = cellIndex;
+    }
+
+    public void evolve() {
+        configuration.setCharAt(
+                cellIndex,
+                cellRule.applyRuleOnAt(
+                        configuration.toString(),
+                        cellIndex
+                ).charAt(0)
+        );
+    }
 }
